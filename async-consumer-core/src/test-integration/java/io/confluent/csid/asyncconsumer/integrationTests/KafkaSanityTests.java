509e2c8927b58e55191f9b927691640e05ebbd7c
Antony Stubbs <antony.stubbs@gmail.com>
2020-08-04 08:17:31 +0100

    Fix broker startup time allowance

--- async-consumer-core/src/test-integration/java/io/confluent/csid/asyncconsumer/integrationTests/KafkaSanityTests.java
+++ async-consumer-core/src/test-integration/java/io/confluent/csid/asyncconsumer/integrationTests/KafkaSanityTests.java
@@ -21,19 +21,28 @@ public class KafkaSanityTests extends KafkaTest<String, String> {
     /**
      * @see io.confluent.csid.asyncconsumer.BrokerPollSystem#pollBrokerForRecords
      */
-    @Timeout(value = 5)
+    @Timeout(value = 20) // includes docker broker startup time, can be slow on CI machines
     @Test
     public void pausedConsumerStillLongPollsForNothing() {
+        log.info("Setup topic");
         setupTopic();
         KafkaConsumer<String, String> c = kcu.consumer;
+        log.info("Subscribe to topic");
         c.subscribe(List.of(topic));
         Set<TopicPartition> assignment = c.assignment();
+        log.info("Pause subscription");
         c.pause(assignment);
+
+        log.info("Initial poll that can trigger some actions and take longer than expected");
+        c.poll(ofSeconds(0));
+
+        log.info("Second poll which is measured");
         Duration longPollTime = ofSeconds(1);
         Duration time = time(() -> {
             c.poll(longPollTime);
         });
-        log.debug("Poll blocked my thread for {}, hopefully longer than {}", time, longPollTime);
+
+        log.info("Poll blocked my thread for {}, hopefully slightly longer than {}", time, longPollTime);
         String desc = "Even though the consumer is paused ALL it's subscribed partitions, it will still perform a long poll against the server";
         var timePlusFluctuation = time.plus(ofMillis(100));
         assertThat(timePlusFluctuation).as(desc)
