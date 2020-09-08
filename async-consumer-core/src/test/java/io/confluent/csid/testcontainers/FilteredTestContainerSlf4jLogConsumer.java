4727ac4210e38a3fff1c105063b91c938718b63f
Antony Stubbs <antony.stubbs@gmail.com>
2020-09-08 16:32:44 +0100

    Enable/disable test container logging

--- async-consumer-core/src/test/java/io/confluent/csid/testcontainers/FilteredTestContainerSlf4jLogConsumer.java
+++ async-consumer-core/src/test/java/io/confluent/csid/testcontainers/FilteredTestContainerSlf4jLogConsumer.java
@@ -6,6 +6,7 @@ package io.confluent.csid.testcontainers;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.event.Level;
 import org.testcontainers.containers.output.OutputFrame;
@@ -19,29 +20,34 @@ import static org.slf4j.event.Level.TRACE;
 
 /**
  * Filters out some log levels from the test container (e.g. Kafka's container has TRACE level set by default).
+ *
+ * Enable logging for this class to get log statements from docker.
  */
-public class FilteredSlf4jLogConsumer extends Slf4jLogConsumer {
+@Slf4j
+public class FilteredTestContainerSlf4jLogConsumer extends Slf4jLogConsumer {
 
     @Getter
     @Setter
     private List<Level> filteredLevels = UniLists.of(TRACE, DEBUG);
 
-    public FilteredSlf4jLogConsumer(Logger logger) {
+    public FilteredTestContainerSlf4jLogConsumer(Logger logger) {
         super(logger);
     }
 
-    public FilteredSlf4jLogConsumer(Logger logger, boolean separateOutputStreams) {
+    public FilteredTestContainerSlf4jLogConsumer(Logger logger, boolean separateOutputStreams) {
         super(logger, separateOutputStreams);
     }
 
     @Override
     public void accept(OutputFrame outputFrame) {
-        String utf8String = outputFrame.getUtf8String();
-        boolean isFilteredOut = filteredLevels.stream().anyMatch(level -> utf8String.contains(level.toString()));
-        if (!isFilteredOut) {
-            super.accept(outputFrame);
-        } else {
-            // ignoring trace level logging
+        if (log.isDebugEnabled()) {
+            String utf8String = outputFrame.getUtf8String();
+            boolean isFilteredOut = filteredLevels.stream().anyMatch(level -> utf8String.contains(level.toString()));
+            if (!isFilteredOut) {
+                super.accept(outputFrame);
+            } else {
+                // ignoring trace level logging
+            }
         }
     }
 }
