5f837f7043af0f7ab0245fd93bbceeed94f19ec1
Antony Stubbs <antony.stubbs@gmail.com>
2022-03-23 09:49:17 +0000

    license

--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/PollContextInternal.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/PollContextInternal.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import io.confluent.parallelconsumer.state.WorkContainer;
 import lombok.Getter;
 import lombok.experimental.Delegate;
--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/state/ConsumerRecordId.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/state/ConsumerRecordId.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer.state;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import lombok.Value;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
