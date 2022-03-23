2f453e903aed211a2088f6d6407ef98171ae3a09
Antony Stubbs <antony.stubbs@gmail.com>
2022-03-23 15:34:03 +0000

    license

--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/ParallelConsumerException.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/ParallelConsumerException.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 
 /**
  * Generic Parallel Consumer {@link RuntimeException} parent.
--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/offsets/EncodingNotSupportedException.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/offsets/EncodingNotSupportedException.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer.offsets;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import io.confluent.parallelconsumer.internal.ParallelConsumerInternalException;
 
 /*-
