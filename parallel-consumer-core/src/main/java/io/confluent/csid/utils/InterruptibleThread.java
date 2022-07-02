3e7d364453fcf3afb76b471a2bf9b5fd6f59280c
Antony Stubbs <antony.stubbs@gmail.com>
2022-07-02 18:01:49 +0300

    better logging and fix reason

--- parallel-consumer-core/src/main/java/io/confluent/csid/utils/InterruptibleThread.java
+++ parallel-consumer-core/src/main/java/io/confluent/csid/utils/InterruptibleThread.java
@@ -5,10 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.event.Level;
 
+import java.util.Optional;
+
 import static io.confluent.csid.utils.StringUtils.msg;
+import static java.util.Optional.of;
 
 /**
- * todo docs
+ * todo docs, order class elements
  *
  * @author Antony Stubbs
  */
@@ -18,10 +21,13 @@ public class InterruptibleThread {
 
     final Thread thread;
 
-    @Getter
-    static final ThreadLocal<Reason> objectThreadLocal = new ThreadLocal<>();
+//    @Getter
+//    static final ThreadLocal<Reason> objectThreadLocal = new ThreadLocal<>();
+//    static final ThreadLocal<Reason> objectThreadLocal = new ThreadLocal<>();
+//
 
-    Reason interruptReason = Reason.UNKNOWN;
+    // todo make not static?
+    private static Reason interruptReason = Reason.UNKNOWN;
 
     public static void logInterrupted(Logger delegateLogger, InterruptedException e) {
         logInterrupted(delegateLogger, "Interrupted", e);
@@ -37,13 +43,13 @@ public class InterruptibleThread {
 
     public static void logInterrupted(Logger delegateLogger, Level level, String msg, InterruptedException e) {
         //
-        String locationString = getLocationString();
+        Optional<String> locationString = getLocationString();
         Reason reason = getInterruptReasonTL();
 
         //
         var reasonNormalised = reason == null ? "No reason given" : "Reason: " + reason;
         var msgNormalised = msg == null ? "" : (msg + ". ");
-        var msgCombined = msg("{}{}. {}", msgNormalised, reasonNormalised, locationString);
+        var msgCombined = msg("{}{}. {}", msgNormalised, reasonNormalised, locationString.orElse("Can't find location"));
 
         // only Slf4j2 allows for dynamic levels - so use our own for now, until slf4j2 is widely adopted, if ever
         switch (level) {
@@ -58,24 +64,26 @@ public class InterruptibleThread {
         Thread.currentThread().interrupt();
     }
 
-    private static String getLocationString() {
-        StackTraceElement callerData = getCallerData();
-        String fileName = callerData.getFileName();
-        int lineNumber = callerData.getLineNumber();
-        String methodName = callerData.getMethodName();
-        return msg("({}:{}#{})", fileName, lineNumber, methodName);
+    private static Optional<String> getLocationString() {
+        Optional<StackTraceElement> callerData = getCallerData();
+        return callerData.map(stackTraceElement -> {
+            String fileName = stackTraceElement.getFileName();
+            int lineNumber = stackTraceElement.getLineNumber();
+            String methodName = stackTraceElement.getMethodName();
+            return msg("({}:{}#{})", fileName, lineNumber, methodName);
+        });
     }
 
-    private static StackTraceElement getCallerData() {
+    private static Optional<StackTraceElement> getCallerData() {
         Throwable cd = new Throwable();
         StackTraceElement[] stackTrace = cd.getStackTrace();
-        StackTraceElement caller = null;
+        Optional<StackTraceElement> caller = Optional.empty();
         Class<InterruptibleThread> clazz = InterruptibleThread.class;
         for (StackTraceElement stackTraceElement : stackTrace) {
             String className = stackTraceElement.getClassName();
             boolean contains = className.contains(clazz.getCanonicalName());
             if (!contains) {
-                caller = stackTraceElement;
+                caller = of(stackTraceElement);
                 break;
             }
         }
@@ -83,12 +91,18 @@ public class InterruptibleThread {
     }
 
     private static Reason getInterruptReasonTL() {
-        return objectThreadLocal.get();
+        var reason = interruptReason;
+        clearReason();
+        return reason;
+    }
+
+    private static void clearReason() {
+        interruptReason = Reason.UNKNOWN;
     }
 
     public void interrupt(Logger delegateLogger, Reason interruptReason) {
-        this.interruptReason = interruptReason;
-        objectThreadLocal.set(interruptReason);
+        InterruptibleThread.interruptReason = interruptReason;
+//        objectThreadLocal.set(interruptReason);
         delegateLogger.debug("Interrupting thread {} for: {}", thread.getName(), interruptReason);
         thread.interrupt();
     }
