2766db94fb45125aeabca5f77a31858941b7facf
Antony Stubbs <antony.stubbs@gmail.com>
2022-07-02 17:29:58 +0300

    log location and msg normalisation

--- parallel-consumer-core/src/main/java/io/confluent/csid/utils/InterruptibleThread.java
+++ parallel-consumer-core/src/main/java/io/confluent/csid/utils/InterruptibleThread.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.event.Level;
 
+import static io.confluent.csid.utils.StringUtils.msg;
+
 /**
  * todo docs
  *
@@ -34,7 +36,15 @@ public class InterruptibleThread {
     }
 
     public static void logInterrupted(Logger delegateLogger, Level level, String msg, InterruptedException e) {
-        String msgCombined = msg + ": " + getInterruptReasonTL();
+        //
+        String locationString = getLocationString();
+        Reason reason = getInterruptReasonTL();
+
+        //
+        var reasonNormalised = reason == null ? "No reason given" : "Reason: " + reason;
+        var msgNormalised = msg == null ? "" : (msg + ". ");
+        var msgCombined = msg("{}{}. {}", msgNormalised, reasonNormalised, locationString);
+
         // only Slf4j2 allows for dynamic levels - so use our own for now, until slf4j2 is widely adopted, if ever
         switch (level) {
             case ERROR -> delegateLogger.error(msgCombined, e);
@@ -43,6 +53,33 @@ public class InterruptibleThread {
             case DEBUG -> delegateLogger.debug(msgCombined, e);
             case TRACE -> delegateLogger.trace(msgCombined, e);
         }
+
+        //
+        Thread.currentThread().interrupt();
+    }
+
+    private static String getLocationString() {
+        StackTraceElement callerData = getCallerData();
+        String fileName = callerData.getFileName();
+        int lineNumber = callerData.getLineNumber();
+        String methodName = callerData.getMethodName();
+        return msg("({}:{}#{})", fileName, lineNumber, methodName);
+    }
+
+    private static StackTraceElement getCallerData() {
+        Throwable cd = new Throwable();
+        StackTraceElement[] stackTrace = cd.getStackTrace();
+        StackTraceElement caller = null;
+        Class<InterruptibleThread> clazz = InterruptibleThread.class;
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            String className = stackTraceElement.getClassName();
+            boolean contains = className.contains(clazz.getCanonicalName());
+            if (!contains) {
+                caller = stackTraceElement;
+                break;
+            }
+        }
+        return caller;
     }
 
     private static Reason getInterruptReasonTL() {
@@ -68,9 +105,5 @@ public class InterruptibleThread {
         public static final Reason UNKNOWN = new Reason("unknown-reason");
         private final String desc;
         private Reason root;
-
-//        public Reason(String msg, Reason reason) {
-//            this()
-//        }
     }
 }
