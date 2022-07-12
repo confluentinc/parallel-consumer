311ce83376fbd36345207beee4ba4f05158c9929
Antony Stubbs <antony.stubbs@gmail.com>
2022-07-12 12:58:35 +0100

    ide: Add in ignored bisect helper and run targets

---
+++ bin/checkcompile-license.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+COMMITS=$(git log --oneline HEAD...parralel-test-fix^ | cut -d " " -f 1)
+
+testCommit() {
+  # COMMIT = $1
+  echo Checking out commit $COMMIT
+  git checkout $COMMIT >/dev/null 2>/dev/null
+
+  #    mvn compile test-compile > /dev/null 2> /dev/null
+  mvn license:check
+
+  if [ $? -eq 0 ]; then
+    echo $COMMIT passed
+  else
+    echo $COMMIT failed
+  fi
+}
+
+for COMMIT in $COMMITS; do
+  testCommit "$COMMIT"
+done
