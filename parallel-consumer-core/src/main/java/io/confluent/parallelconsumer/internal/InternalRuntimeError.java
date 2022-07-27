2c70d112f14284e9ca42a88371dbb58a345967c8
Antony Stubbs <antony.stubbs@gmail.com>
2022-07-27 10:57:18 +0100

    license

--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/internal/InternalRuntimeError.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/internal/InternalRuntimeError.java
@@ -1,9 +1,13 @@
 package io.confluent.parallelconsumer.internal;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import lombok.experimental.StandardException;
 
-/*-
- * Copyright (C) 2020-2021 Confluent, Inc.
+/**
+ * Generic internal runtime error
  */
 @StandardException
 public class InternalRuntimeError extends RuntimeException {
