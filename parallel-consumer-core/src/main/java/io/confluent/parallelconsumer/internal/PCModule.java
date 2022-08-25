41dde0fec7c7574f2b7593bad60891121d94b5f7
Antony Stubbs <antony.stubbs@gmail.com>
2022-08-25 11:01:49 +0100

    license

--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/internal/PCModule.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/internal/PCModule.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer.internal;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import io.confluent.csid.utils.TimeUtils;
 import io.confluent.parallelconsumer.ParallelConsumerOptions;
 import io.confluent.parallelconsumer.ParallelEoSStreamProcessor;
