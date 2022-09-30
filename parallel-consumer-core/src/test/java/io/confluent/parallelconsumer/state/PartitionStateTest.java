d1c42d692ddf8b3e152914d7012be5f76a576528
Antony Stubbs <antony.stubbs@gmail.com>
2022-09-30 16:40:47 +0100

    review

--- parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/state/PartitionStateTest.java
+++ parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/state/PartitionStateTest.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer.state;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import com.google.common.truth.Truth;
 import io.confluent.parallelconsumer.internal.PCModuleTestEnv;
 import io.confluent.parallelconsumer.offsets.OffsetMapCodecManager.HighestOffsetAndIncompletes;
