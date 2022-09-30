b456cdc5ffc14beef1a3503f1332da9b687ff504
Antony Stubbs <antony.stubbs@gmail.com>
2022-09-30 14:20:17 +0100

    review

--- parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/state/ShardManagerTest.java
+++ parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/state/ShardManagerTest.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer.state;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import io.confluent.parallelconsumer.internal.PCModuleTestEnv;
 import org.junit.jupiter.api.Test;
 
