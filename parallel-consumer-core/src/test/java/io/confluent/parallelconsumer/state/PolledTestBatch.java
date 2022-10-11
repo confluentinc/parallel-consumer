747b132dd9b2bf7bcd961b435f53112d5140093e
Antony Stubbs <antony.stubbs@gmail.com>
2022-10-11 13:02:22 +0100

    review

--- parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/state/PolledTestBatch.java
+++ parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/state/PolledTestBatch.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer.state;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import io.confluent.csid.utils.JavaUtils;
 import io.confluent.parallelconsumer.internal.EpochAndRecordsMap;
 import one.util.streamex.LongStreamEx;
