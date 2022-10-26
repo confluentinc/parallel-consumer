6a17c50900b949a4d1da208569e97a1b73e465b4
Robbie Palmer <8760191+Robbie-Palmer@users.noreply.github.com>
2022-10-26 11:36:25 +0100

    Take mvnw from repository root on build

--- parallel-consumer-python/setup.py
+++ parallel-consumer-python/setup.py
@@ -6,18 +6,28 @@ from pathlib import Path
 from string import Template
 from xml.etree.ElementTree import parse as parse_xml
 from setuptools import setup, find_packages
+import shutil
 
 with open('requirements.txt', 'r', encoding='utf-8') as requirements_file:
     requirements = requirements_file.readlines()
 
 version = '0.1.0a0'
 
-pc_pom_path = Path(__file__).parent.parent / 'pom.xml'
+project_root = Path(__file__).parent
+repo_root = project_root.parent
+python_package_path = project_root / 'pyallel_consumer'
+pc_pom_path = repo_root / 'pom.xml'
 pc_version = parse_xml(pc_pom_path).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text
 pom_template = Template(Path('./pom-template.xml').read_text())
 pom = pom_template.substitute(package_version=version,
                               pc_version=pc_version)
-Path('./pyallel_consumer/pom.xml').write_text(pom)
+python_package_path.joinpath('pom.xml').write_text(pom)
+
+# Can't be a hidden dir with preceding . or setuptools won't find it
+shutil.copytree(repo_root / '.mvn', python_package_path / 'mvn', dirs_exist_ok=True)
+copy_file_into_package = lambda file_name: shutil.copy(repo_root / file_name, python_package_path / file_name)
+shutil.copy(repo_root / 'mvnw', python_package_path)
+shutil.copy(repo_root / 'mvnw.cmd', python_package_path)
 
 setup(
     name='pyallel_consumer',
