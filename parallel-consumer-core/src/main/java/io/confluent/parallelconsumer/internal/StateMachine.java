c867f8abe38243679f0cfea47c986eb1ee953ae2
Antony Stubbs <antony.stubbs@gmail.com>
2022-12-02 13:02:01 +0000

    license

--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/internal/StateMachine.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/internal/StateMachine.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer.internal;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
