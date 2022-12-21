5ca7432289c20e8c44011eb3a84aa6a77012cb4c
Antony Stubbs <antony.stubbs@gmail.com>
2022-12-21 03:17:17 +0000

    license

--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/ConsumerApiAccess.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/ConsumerApiAccess.java
@@ -1,5 +1,9 @@
 package io.confluent.parallelconsumer;
 
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
 import io.confluent.parallelconsumer.internal.ConsumerFacade;
 import io.confluent.parallelconsumer.internal.FullConsumerFacade;
 import io.confluent.parallelconsumer.internal.PCConsumerAPI;
