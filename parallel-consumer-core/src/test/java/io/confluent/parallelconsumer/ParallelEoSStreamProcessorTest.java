c7a8d7eb40c61b4e8cc5fe149175351694a42702
Antony Stubbs <antony.stubbs@gmail.com>
2022-12-21 13:49:30 +0000

    START: tests: Fix lessKeysThanThreads time out

--- parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/ParallelEoSStreamProcessorTest.java
+++ parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/ParallelEoSStreamProcessorTest.java
@@ -866,10 +866,10 @@ public class ParallelEoSStreamProcessorTest extends ParallelEoSStreamProcessorTe
         // use a small set of keys, over a large set of records
         final int keySetSize = 4;
         var keys = Range.range(keySetSize).listAsIntegers();
-        final int total = 100_000;
+        final int total = 10_000;
         log.debug("Generating {} records against {} keys...", total, keySetSize);
         var records = ktu.generateRecords(keys, total);
-        records.entrySet().forEach(x -> log.debug("Key {} has {} records", x.getKey(), x.getValue().size()));
+        records.forEach((key, value) -> log.debug("Key {} has {} records", key, value.size()));
         log.debug("Sending...");
         ktu.send(consumerSpy, records);
 
