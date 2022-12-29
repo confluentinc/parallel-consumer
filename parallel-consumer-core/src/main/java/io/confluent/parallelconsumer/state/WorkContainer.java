6f6d7fb4a1920425a3dfb66649c18c3945c5a289
Antony Stubbs <antony.stubbs@gmail.com>
2022-12-29 15:06:59 +0000

    log improvements

--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/state/WorkContainer.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/state/WorkContainer.java
@@ -201,7 +201,7 @@ public class WorkContainer<K, V> implements Comparable<WorkContainer<K, V>> {
 
     @Override
     public String toString() {
-        return "WorkContainer(" + toTopicPartition(cr) + ":" + cr.offset() + ":" + cr.key() + ")";
+        return "WorkContainer(tp:" + toTopicPartition(cr) + ":o:" + cr.offset() + ":k:" + cr.key() + ")";
     }
 
     public Duration getTimeInFlight() {
