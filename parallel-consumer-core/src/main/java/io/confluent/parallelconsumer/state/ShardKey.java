6edc9ba375a4ec6f51bee41d286d27d4c58ecf17
Roman Kolesnev <rkolesnev@confluent.io>
2023-09-07 13:32:37 +0100

    PL-354 - fix key equality

--- parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/state/ShardKey.java
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/state/ShardKey.java
@@ -10,6 +10,9 @@ import lombok.experimental.FieldDefaults;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Simple value class for processing {@link ShardKey}s to make the various key systems type safe and extendable.
  *
@@ -41,7 +44,6 @@ public class ShardKey {
     }
 
     @Value
-    @RequiredArgsConstructor
     @EqualsAndHashCode(callSuper = true)
     public static class KeyOrderedKey extends ShardKey {
 
@@ -54,12 +56,92 @@ public class ShardKey {
         /**
          * The key of the record being referenced. Nullable if record is produced with a null key.
          */
-        Object key;
+        KeyWithEquals key;
 
         public KeyOrderedKey(final ConsumerRecord<?, ?> rec) {
             this(new TopicPartition(rec.topic(), rec.partition()), rec.key());
         }
+
+        public KeyOrderedKey(final TopicPartition topicPartition, final Object key) {
+            if (key instanceof KeyWithEquals) {
+                this.key = (KeyWithEquals) key;
+            } else {
+                this.key = new KeyWithEquals(key);
+            }
+            this.topicName = topicPartition;
+        }
     }
+
+    @Value
+    @RequiredArgsConstructor
+    public static class KeyWithEquals {
+        Object key;
+
+        @Override
+        public boolean equals(Object o) {
+
+            if (o == this) return true;
+            if (!(o instanceof KeyWithEquals)) return false;
+            KeyWithEquals other = (KeyWithEquals) o;
+            if (other.key == null && this.key == null) return true;
+            if (other.key == null || this.key == null) return false;
+            return Objects.deepEquals(this.key, other.key);
+
+        }
+
+        @Override
+        public int hashCode() {
+            final int PRIME = 59;
+            int result = 1;
+            result = (result * PRIME);
+            if (key == null) {
+                result = result + 43;
+                return result;
+            }
+            if (isArray(key)) {
+                result = result + arrayHashCode(key);
+            }
+            return result;
+        }
+
+        private int arrayHashCode(Object t) {
+            if (t instanceof Object[]) {
+                return Arrays.deepHashCode((Object[]) t);
+            } else {
+                return primitiveArrayHashCode(t, t.getClass().getComponentType());
+            }
+        }
+
+        /**
+         * Copy of {@link Arrays#primitiveArrayHashCode} logic
+         *
+         * @param a
+         * @param cl
+         * @return
+         */
+        private int primitiveArrayHashCode(Object a, Class<?> cl) {
+            return
+                    (cl == byte.class) ? Arrays.hashCode((byte[]) a) :
+                            (cl == int.class) ? Arrays.hashCode((int[]) a) :
+                                    (cl == long.class) ? Arrays.hashCode((long[]) a) :
+                                            (cl == char.class) ? Arrays.hashCode((char[]) a) :
+                                                    (cl == short.class) ? Arrays.hashCode((short[]) a) :
+                                                            (cl == boolean.class) ? Arrays.hashCode((boolean[]) a) :
+                                                                    (cl == double.class) ? Arrays.hashCode((double[]) a) :
+                                                                            // If new primitive types are ever added, this method must be
+                                                                            // expanded or we will fail here with ClassCastException.
+                                                                            Arrays.hashCode((float[]) a);
+        }
+
+        private boolean isArray(Object obj) {
+            return obj instanceof Object[] || obj instanceof boolean[] ||
+                    obj instanceof byte[] || obj instanceof short[] ||
+                    obj instanceof char[] || obj instanceof int[] ||
+                    obj instanceof long[] || obj instanceof float[] ||
+                    obj instanceof double[];
+        }
+    }
+
 
     @Value
     @EqualsAndHashCode(callSuper = true)
