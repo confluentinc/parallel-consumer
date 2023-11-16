0c9b7da5ee2c02b1f90b3aa56a309f568fb906fb
cloudhunter89 <litereader@gmail.com>
2023-11-16 04:28:29 -0500

    Remove duplicated batchSize factor from target load; add a test case (#662)
    * Remove duplicated batchSize factor from target load; add a test case

---
+++ parallel-consumer-core/src/test/java/io/confluent/parallelconsumer/AbstractParallelEoSStreamProcessorConfigurationTest.java
@@ -0,0 +1,46 @@
+package io.confluent.parallelconsumer;
+
+/*-
+ * Copyright (C) 2020-2023 Confluent, Inc.
+ */
+
+import io.confluent.parallelconsumer.internal.TestParallelEoSStreamProcessor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.clients.consumer.MockConsumer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests to verify the protected and internal methods of
+ * {@link io.confluent.parallelconsumer.internal.AbstractParallelEoSStreamProcessor} work as expected.
+ * <p>
+ *
+ * @author Jonathon Koyle
+ */
+@Slf4j
+class AbstractParallelEoSStreamProcessorConfigurationTest {
+
+    /**
+     * Test that the {@link io.confluent.parallelconsumer.internal.AbstractParallelEoSStreamProcessor#getQueueTargetLoaded}
+     */
+    @Test
+    void queueTargetLoad() {
+        final int batchSize = 10;
+        final int concurrency = 2;
+        final MockConsumer<String, String> consumer = new MockConsumer<>(OffsetResetStrategy.LATEST);
+        final ParallelConsumerOptions<String, String> testOptions = ParallelConsumerOptions.<String, String>builder()
+                .batchSize(batchSize)
+                .maxConcurrency(concurrency)
+                .consumer(consumer)
+                .build();
+        try (final TestParallelEoSStreamProcessor<String, String> testInstance = new TestParallelEoSStreamProcessor<>(testOptions)) {
+            final int defaultLoad = 2;
+            final int expectedTargetLoad = batchSize * concurrency * defaultLoad;
+
+            final int actualTargetLoad = testInstance.getTargetLoad();
+
+            Assertions.assertEquals(expectedTargetLoad, actualTargetLoad);
+        }
+    }
+}
